59d94ad64ead4ac9d8a4b9ec9762ebc45df5b057
yutatanomoki <yuta3071@gmail.com>
2021-10-18 06:59:06 +0900

    [fixed]id削除

--- db/migrate/20211017213019_create_ideas.rb
+++ db/migrate/20211017213019_create_ideas.rb
@@ -1,8 +1,7 @@
 class CreateIdeas < ActiveRecord::Migration[5.2]
   def change
     create_table :ideas do |t|
-      t.bigint :id
-      t.bigint :category_id
+      t.bigint :category_id, null: false
       t.text :body
 
       t.timestamps
