89cd09127cf62ebe9752c9dd240552033ce57330
yutatanomoki <yuta3071@gmail.com>
2021-10-18 09:57:42 +0900

    [add]index,create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,21 @@
 class CategoriesController < ApplicationController
+  def index
+    categorys = Category.all
+    render json: categorys, each_serializer: CategorySerializer
+  end
+
+  def create
+    category = Category.new(category_params)
+    if catgory.save
+      render json: { status: 'SUCCESS', data: category }
+    else
+      render json: { status: 'ERROR', data: category.errors }
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
