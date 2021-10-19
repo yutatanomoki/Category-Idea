addb3dcca089214ecc178fa07a8ffdceddf412f4
yutatanomoki <yuta3071@gmail.com>
2021-10-19 11:50:09 +0900

    [fixed]index

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,25 @@
 class IdeasController < ApplicationController
+  def index
+    ideas = idea.all
+    render json: @ideas
+  end
+
+  def show
+    ideas = Idea.find(params[:id])
+    category =  idea.category
+    render json: @ideas
+  end
+
   def create
     idea = Idea.new(idea_params)
-    if catgory.save
+    if idea.save
       render json: { status: 'SUCCESS', data: idea }
     else
       render json: { status: 'ERROR', data: idea.errors }
     end
+  end
+
+  def idea_params
+    params.require(:idea).permit(:body)
   end
 end
